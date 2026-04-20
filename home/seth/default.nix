350e669d01c0475894e2505ed838cac7593dd536
Seth McLean <seth-mclean@idexx.com>
2026-04-20 20:01:34 +0000

    fix: manage .config sub dirs instead

--- home/seth/default.nix
+++ home/seth/default.nix
@@ -16,8 +16,52 @@
       source = dotfiles + "/bin";
       recursive = true;
     };
-    ".config" = {
-      source = dotfiles + "/.config";
+    ".config/bottom" = {
+      source = dotfiles + "/.config/bottom";
+      recursive = true;
+    };
+    ".config/fd" = {
+      source = dotfiles + "/.config/fd";
+      recursive = true;
+    };
+    ".config/git" = {
+      source = dotfiles + "/.config/git";
+      recursive = true;
+    };
+    ".config/k9s" = {
+      source = dotfiles + "/.config/k9s";
+      recursive = true;
+    };
+    ".config/kind" = {
+      source = dotfiles + "/.config/kind";
+      recursive = true;
+    };
+    ".config/nvim" = {
+      source = dotfiles + "/.config/nvim";
+      recursive = true;
+    };
+    ".config/opencode" = {
+      source = dotfiles + "/.config/opencode";
+      recursive = true;
+    };
+    ".config/ruff" = {
+      source = dotfiles + "/.config/ruff";
+      recursive = true;
+    };
+    ".config/tmux" = {
+      source = dotfiles + "/.config/tmux";
+      recursive = true;
+    };
+    ".config/ty" = {
+      source = dotfiles + "/.config/ty";
+      recursive = true;
+    };
+    ".config/wezterm" = {
+      source = dotfiles + "/.config/wezterm";
+      recursive = true;
+    };
+    ".config/zsh" = {
+      source = dotfiles + "/.config/zsh";
       recursive = true;
     };
   };
